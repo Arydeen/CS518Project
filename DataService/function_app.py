eae36f5a05c3d8301aefb3eb4ffdc98b304ba96c
ajr1180 <alec.rydeen@unh.edu>
2023-12-08 13:53:38 -0500

    Replace function_app.py

--- DataService/function_app.py
+++ DataService/function_app.py
@@ -11,7 +11,6 @@ data_manager.initialize()
 @app.route(route="CreateRecord", methods = ['POST'])
 def CreateRecord(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    print("Inside CreateRecord")
     indoc = req.get_json()
     if not indoc:
         try:
@@ -29,17 +28,45 @@ def CreateRecord(req: func.HttpRequest) -> func.HttpResponse:
              status_code=200
         )
     
-# @app.route(route="ReadRecords", methods = ['GET'])
-# def ReadRecords(req: func.HttpRequest) -> func.HttpResponse:
+@app.route(route="DeleteRecord", methods = ['DELETE'])
+def DeleteRecord(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request')
+    indoc = req.get_json()
+    if not indoc:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            indoc = req_body.get('indoc')
+    if indoc:
+        data_manager.delete(indoc)
+        return func.HttpResponse(f"Record Deleted", status_code=200)
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a document in the query string or in the request body to create a record",
+             status_code=200
+        )
+    
+# @app.route(route="UpdateRecord", methods = ['PUT'])
+# def UpdateRecord(req: func.HttpRequest) -> func.HttpResponse:
 #     logging.info('Python HTTP trigger function processed a request.')
-
-#     name = req.params.get('query')
-#     if not name:
-#         doc = data_manager.read({})
-#         return func.HttpResponse(json_util.dumps(doc))
+#     indoc = req.get_json()
+#     if not indoc:
+#         try:
+#             req_body = req.get_json()
+#         except ValueError:
+#             pass
+#         else:
+#             indoc = req_body.get('indoc')
+#     if indoc:
+#         data_manager.update(indoc)
+#         return func.HttpResponse(f"Record added.", status_code=200)
 #     else:
-#         doc = data_manager.read(json_util.loads(name))
-#         return func.HttpResponse(json_util.dumps(doc))
+#         return func.HttpResponse(
+#              "This HTTP triggered function executed successfully. Pass a document in the query string or in the request body to create a record",
+#              status_code=200
+#         )
 
 @app.route(route="ReadRecords", methods = ['GET'])
 def ReadRecords(req: func.HttpRequest) -> func.HttpResponse:
